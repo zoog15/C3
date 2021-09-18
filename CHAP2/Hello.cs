4c91de0d75aa9cd0a34e84f7c32a063695b0a269
KimMinKwan <zoog15@naver.com>
2021-09-18 21:46:30 +0900

    처음 만들어보는 C#

---
+++ CHAP2/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C3_Practice
+{
+    class Hello
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("여러분 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+        }
+    }
+}
---
+++ CHAP2/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace C샵
+{
+    class MainApp
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
