0108d9185b7897761749e7e0e60f69935eed6198
KimMinKwan <zoog15@naver.com>
2021-09-19 21:59:37 +0900

    데이터 보관하기

---
+++ CHAP3/문자열숫자형변환.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C3_Practice
+{
+    class StringNumberConversion
+    {
+        static void Main(string[] args) {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "123456";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.2345";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
+        }
+    }
+}
