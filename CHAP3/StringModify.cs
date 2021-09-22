2d56c21daf96d8ee1b7d0ec9cad9f15a59a4faa5
KimMinKwan <zoog15@naver.com>
2021-09-22 21:54:49 +0900

    데이터 보관하기

---
+++ CHAP3/StringModify.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace C3_Practice
+{
+    class StringModify
+    {
+
+        static void Main(string[] args) {
+            WriteLine("ToLower() : '{0}'", "ABC".ToLower());
+            WriteLine("ToUpper() : '{0}'", "abc".ToUpper());
+
+            WriteLine("Insert() : '{0}'", "Happy Friday!".Insert(5, " Sunny"));
+            WriteLine("Remove() : '{0}'", "I Don't Love You.".Remove(2, 6));
+
+            WriteLine("Trim() : '{0}'", " No Spaces ".Trim());
+            WriteLine("TrimStart() : '{0}'", " No Spaces ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", " No Spaces ".TrimEnd());
+        }
+    }
+}
---
+++ CHAP3/StringSlice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace C3_Practice
+{
+    class StringSlice
+    {
+        static void Main(string[] args) {
+            string greeting = "Good morning";
+
+            WriteLine(greeting.Substring(0, 5)); // "Good"
+            WriteLine(greeting.Substring(5)); // "morning"
+            WriteLine();
+
+            string[] arr = greeting.Split(new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+        }
+    }
+}
