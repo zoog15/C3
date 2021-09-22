b20fde221a4dd07126eab439de506c1d34905318
KimMinKwan <zoog15@naver.com>
2021-09-22 20:30:12 +0900

    데이터 보관하기

---
+++ CHAP3/StringSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace C3_Practice
+{
+    class StringSearch
+    {
+
+        static void Main(string[] args) {
+            string greeting = "Good Morning";
+
+            WriteLine(greeting);
+            WriteLine();
+
+            // IndexOf()
+            WriteLine("IndexOf 'Good' : {0}", greeting.IndexOf("Good"));
+            WriteLine("IndexOf 'o' : {0}", greeting.IndexOf('o'));
+
+            // LastIndexOf()
+            WriteLine("LastIndexOf 'Good' : {0}", greeting.LastIndexOf("Good"));
+            WriteLine("LastIndexOf 'o' : {0}", greeting.LastIndexOf('o'));
+
+            // StartsWith()
+            WriteLine("StartsWith 'Good' : {0}", greeting.StartsWith("Good"));
+            WriteLine("StartsWith 'Morning' : {0}", greeting.StartsWith("Morning"));
+
+            // EndsWith()
+            WriteLine("EndsWith 'Good' : {0}", greeting.EndsWith("Good"));
+            WriteLine("EndsWith 'Morning' : {0}", greeting.EndsWith("Morning"));
+
+            // Contains()
+            WriteLine("Contains 'Evening' : {0}", greeting.Contains("Evening"));
+            WriteLine("Contains 'Morning' : {0}", greeting.Contains("Morning"));
+
+            // Replace()
+            WriteLine("Replaced 'Morning' with 'Evening' : {0}", greeting.Replace("Morning", " Evening"));
+        }
+    }
+}
