46049ee0e3a31a4a5f0959c5e787379179e3ab4c
KimMinKwan <zoog15@naver.com>
2021-09-24 14:20:35 +0900

    코드의 흐름을 제어하기

---
+++ CHAP5/Prac1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace C3_Practice
+{
+    class Prac1
+    {
+        static void Main(string[] args) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j <= i; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CHAP5/Prac2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace C3_Practice
+{
+    class Prac2
+    {
+        static void Main(string[] args) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 5; j > i; j--) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
