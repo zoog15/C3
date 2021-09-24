a0c79e60dd39293d4d04df8382aeb06853cb5fcf
KimMinKwan <zoog15@naver.com>
2021-09-24 14:01:33 +0900

    코드의 흐름을 제어하기

---
+++ CHAP5/Break.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace C3_Practice
+{
+    class Break
+    {
+        static void Main(string[] args) {
+            while (true) {
+                Console.Write("계속할까요?(예/아니오) :");
+                string answer = Console.ReadLine();
+
+                if (answer == "아니오")
+                    break;
+            }
+        }
+    }
+}
