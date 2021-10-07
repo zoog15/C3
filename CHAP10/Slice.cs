854836a1d21e6c02c3707ee11596e68b793ce959
KimMinKwan <zoog15@naver.com>
2021-10-07 15:25:10 +0900

    배열 컬렉션 인덱서

---
+++ CHAP10/Slice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace MoreonArray
+{
+    class MainApp {
+        static void PrintArray(System.Array array) {
+            foreach (var e in array)
+                Console.Write(e);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) {
+            char[] array = new char['Z' - 'A' + 1]; // 26
+            for (int i = 0; i < array.Length; i++)
+                array[i] = (char)('A' + i); // array에 'A'에서 'Z'까지 입력
+
+            PrintArray(array[..]); // 0번째 부터 마지막까지
+            PrintArray(array[5..]); // 5번째부터 끝까지
+
+            Range range_5_10 = 5..10;
+            PrintArray(array[range_5_10]); // 5번째부터 9(10-1)번까지
+
+            Index last = ^0;
+            Range range_5_last = 5..last;
+            PrintArray(array[range_5_last]); // 5번째부터 끝(^)까지
+
+            PrintArray(array[^4..^1]); // 뒤에서 4번째부터 끝에서 2번쨰까지
+        }
+    }
+}
