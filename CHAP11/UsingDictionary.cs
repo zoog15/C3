eb525cff2cb5b15bf6c0fec55aa18cff754e9480
KimMinKwan <zoog15@naver.com>
2021-10-08 13:21:11 +0900

    일반화 프로그래밍

---
+++ CHAP11/UsingDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+
+namespace UsingDictionary
+{
+    class MainApp {
+        static void Main(string[] args) {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["하나"] = "one";
+            dic["둘"] = "two";
+            dic["셋"] = "three";
+            dic["넷"] = "four";
+            dic["다섯"] = "five";
+
+            WriteLine(dic["하나"]);
+            WriteLine(dic["둘"]);
+            WriteLine(dic["셋"]);
+            WriteLine(dic["넷"]);
+            WriteLine(dic["다섯"]);
+        }
+    }
+}
