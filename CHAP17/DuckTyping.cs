d2dc7c066312df39e40a6a75c9003833e506baf4
KimMinKwan <zoog15@naver.com>
2021-10-20 09:45:52 +0900

    덕타이핑

---
+++ CHAP17/DuckTyping.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+
+
+
+
+namespace DuckTyping
+{
+    class Duck {
+        public void Walk() {
+            WriteLine(this.GetType() + ".Walk");
+        }
+
+        public void Swim()
+        {
+            WriteLine(this.GetType() + ".Swim");
+        }
+
+        public void Quack() {
+            WriteLine(this.GetType() + ".Quack");
+        }
+
+        class Mallard : Duck { }
+
+        class Robot {
+            public void Walk()
+            {
+                WriteLine("Robot.Walk");
+            }
+
+            public void Swim()
+            {
+                WriteLine("Robot.Swim");
+            }
+
+            public void Quack()
+            {
+                WriteLine("Robot.Quack");
+            }
+        }
+
+        class MainApp {
+            static void Main(string[] args) {
+                dynamic[] arr = new dynamic[] { new Duck(), new Mallard(), new Robot() };
+
+                foreach (dynamic duck in arr) {
+                    WriteLine(duck.GetType());
+                    duck.Walk();
+                    duck.Swim();
+                    duck.Quack();
+
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
