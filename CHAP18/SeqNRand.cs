2a184a3a7b2d451fb2827210312c0fa4ceebad18
KimMinKwan <zoog15@naver.com>
2021-10-22 11:29:26 +0900

    입의접근방식 통한 position 이동

---
+++ CHAP18/SeqNRand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+
+
+
+namespace SeqNRand
+{
+    class MainApp {
+        static void Main(string[] args) {
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.Seek(5, SeekOrigin.Current);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x04);
+            WriteLine($"Position : {outStream.Position}");
+
+            outStream.Close();
+        }
+    }
+}
