7b4638da410cbf585c40e901ef37bf981f5c8c96
KimMinKwan <zoog15@naver.com>
2021-10-22 11:05:51 +0900

    디렉터리/파일 생성

---
+++ CHAP18/Touch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+
+
+
+namespace Touch
+{
+    class MainApp {
+        static void OnWrongPathType(string type) {
+            WriteLine($"{type} is wrong type");
+            return;
+        }
+        static void Main(string[] args) {
+            if (args.Length == 0) {
+                WriteLine("Usage : Touch.exe <Path> [Type:File/Directory]");
+                return;
+            }
+
+            string path = args[0];
+            string type = "File";
+            if (args.Length > 1)
+                type = args[1];
+
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                if (type == "File")
+                    File.SetLastWriteTime(path, DateTime.Now);
+                else if (type == "Directory")
+                    Directory.SetLastWriteTime(path, DateTime.Now);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                WriteLine($"Updated {path} {type}");
+            }
+            else {
+                if (type == "File")
+                    File.Create(path).Close();
+                else if (type == "Directory")
+                    Directory.CreateDirectory(path);
+                else {
+                    OnWrongPathType(path);
+                    return;
+                }
+
+                WriteLine($"Created {path} {type}");
+            }
+        }
+    }
+}
