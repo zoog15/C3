f7961a1ef5dff33c100286c4f5336756352cfdf3
KimMinKwan <zoog15@naver.com>
2021-10-22 10:45:55 +0900

    디렉터리/파일 조회하기

---
+++ CHAP18/Dir.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+using System.IO;
+
+
+
+
+namespace Dir
+{
+    class MainApp {
+        static void Main(string[] args) {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            WriteLine($"{directory} directory Info");
+            WriteLine("- Directories : ");
+            var directories = (from dir in Directory.GetDirectories(directory)
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.Name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach (var d in directories)
+                WriteLine($"{d.Name} : {d.Attributes}");
+
+            WriteLine("-Files :");
+            var files = (from file in Directory.GetFiles(directory)
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+
+            foreach (var f in files)
+                WriteLine($"{f.Name} : {f.FileSize}, {f.Attributes}");
+        }
+    }
+}
