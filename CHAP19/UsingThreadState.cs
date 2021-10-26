d0cee7d64d9261c61816c219d1315515db8e11fe
KimMinKwan <zoog15@naver.com>
2021-10-26 21:25:49 +0900

    스레드의 각종 상태

---
+++ CHAP19/UsingThreadState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+
+
+
+namespace UsingThreadState
+{
+    class MainApp {
+        private static void PrintThreadState(ThreadState state) {
+            WriteLine("{0,-16} : {1}", state, (int)state);
+        }
+
+        static void Main(string[] args) {
+            PrintThreadState(ThreadState.Running);
+
+            PrintThreadState(ThreadState.StopRequested);
+
+            PrintThreadState(ThreadState.SuspendRequested);
+
+            PrintThreadState(ThreadState.Background);
+
+            PrintThreadState(ThreadState.Unstarted);
+
+            PrintThreadState(ThreadState.Stopped);
+
+            PrintThreadState(ThreadState.WaitSleepJoin);
+
+            PrintThreadState(ThreadState.Suspended);
+
+            PrintThreadState(ThreadState.AbortRequested);
+
+            PrintThreadState(ThreadState.Aborted);
+
+            PrintThreadState(ThreadState.Aborted | ThreadState.Stopped);
+        }
+    }
+}
