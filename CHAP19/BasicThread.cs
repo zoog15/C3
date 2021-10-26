551e37497a83405c74c80971db509517420f4017
KimMinKwan <zoog15@naver.com>
2021-10-26 21:10:10 +0900

    기본적인 스레드 사용

---
+++ CHAP19/BasicThread.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+
+
+
+namespace BasicThread
+{
+    class MainApp {
+        static void DoSomething() {
+            for (int i = 0; i < 5; i++) {
+                WriteLine($"DoSomething : {i}");
+                // Sleep() 메서드를 만나면 인수(10) 만큼 CPU 사용을 멈춤. 이떄 단위는 밀리초
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args) {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+            WriteLine("Starting thread...");
+            t1.Start();
+
+            for (int i = 0; i < 5; i++) {
+                WriteLine($"Main : {i}");
+                Thread.Sleep(10);
+            }
+
+            WriteLine("Waiting until thread stops...");
+            t1.Join();
+
+            WriteLine("Finished");
+        }
+    }
+}
