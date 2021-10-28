6ba72a2e0ab14d26d32f7ace3ea47530002faf9c
KimMinKwan <zoog15@naver.com>
2021-10-28 20:39:01 +0900

    스레드를 중지시키는 interrupt

---
+++ CHAP19/InterruptingThread.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+
+
+
+namespace InterruptingThread
+{
+    class SideTask {
+        int count;
+
+        public SideTask(int count) {
+            this.count = count;    
+        }
+
+        public void KeepAlive()
+        {
+            try
+            {
+                WriteLine("Running thread isn't gonna be interrupted");
+                Thread.SpinWait(10000000);
+
+                while (count > 0)
+                {
+                    WriteLine($"{count--} left");
+
+                    WriteLine("Entering into WaitJoinSleep State...");
+                    Thread.Sleep(10);
+                }
+                WriteLine("Count : 0");
+            }
+            catch (ThreadInterruptedException e)
+            {
+                WriteLine(e);
+            }
+            finally {
+                WriteLine("Clearing resource...");
+            }
+        }
+    }
+
+    class MainApp {
+        static void Main(string[] args) {
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
+
+            WriteLine("Starting thread...");
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            WriteLine("Interrupting thread...");
+            t1.Interrupt();
+
+            WriteLine("Waiting until thread stops...");
+            t1.Join();
+
+            WriteLine("Finished");
+        }
+    }
+}
